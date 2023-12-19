d714da7b52a7d0d1822a6ca75fca5c2f591920e5
commieloliqq <commieloliqq@gmail.com>
2023-12-19 20:12:54 +0600

    account start

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'account.CustomUser'
+AUTH_USER_MODEL = 'account.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
