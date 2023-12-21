4b16ade277ec9dcfc46534febca29ebf172593f3
Adilet2628 <adiletraimbekov269@gmail.com>
2023-12-21 13:36:42 +0600

    test

--- Station/models.py
+++ Station/models.py
@@ -2,7 +2,7 @@ from django.db import models
 #from rest_framework
 from ckeditor.fields import RichTextField
 
-
+#asda
 
 class StationImage(models.Model):
     name = models.CharField(max_length=255,  blank=True)
