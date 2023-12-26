43ae6bd808c02a715f2258ad0482cec7428dd221
commieloliqq <commieloliqq@gmail.com>
2023-12-26 06:42:23 +0000

    test5

--- account/utils.py
+++ account/utils.py
@@ -5,13 +5,13 @@ from django.dispatch import receiver
 from django_rest_passwordreset.signals import reset_password_token_created
 
 
-HOST = 'localhost:8000'
+HOST = '34.42.139.23'
 
 
 def send_confirmation_email(user, code):
     link = f'http://{HOST}/api/v1/account/activate/{code}/'
     send_mail(
-        'ANVAR LOX! Здравствуйте, активируйте ваш аккаунт!',
+        'Здравствуйте, активируйте ваш аккаунт!',
         f'Что активировать ваш аккаунт нужно перейти по ссылке ниже:'
         f'\n{link}'
         f'\nСсылка работает один раз!',
@@ -24,7 +24,7 @@ def send_confirmation_email(user, code):
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
     send_mail(
-        'ANVAR PIDOR! Password reset',
+        'Password reset',
         f'Что reset ваш password нужно ввести код ниже:'
         f'\n'
         f'{reset_password_token.key}',
